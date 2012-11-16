7ac4f45db772bc35a7a9000b17e157c64293168d
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-11-15 23:46:04 -0300

    creando una ruta con parametro.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = flask.Flask(__name__)
 def home():
     return "Bienvenidos!!!"
 
+@app.route("/saludar/<nombre>")
+def saludar(nombre):
+    return "Hola <b>{nombre}</b> como estas?".format(nombre=nombre)
+
 if __name__ == '__main__':
     app.run(debug=True)
